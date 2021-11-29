94e8944c9d369a96c7d3b16eb694f99043ddfbbe
Oleg <oleg.irchishyn91@gmail.com>
2021-11-29 17:13:15 +0200

    added server.js file

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import { SideBarTodoListsType, TasksType } from '../redux/types/types';
 
 const instance = axios.create({
   withCredentials: true,
-  baseURL: 'http://localhost:3001/',
+  baseURL: '/',
 });
 
 export const todoAPI = {
