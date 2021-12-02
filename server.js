5d53af5f31fb6ef56249be5f1d36687aafc51b3d
Oleg <oleg.irchishyn91@gmail.com>
2021-12-02 17:43:22 +0200

    reverse latest changes

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({
   static: './build',
 });
+
 const PORT = process.env.PORT || 3001;
+
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    '/api/*': '/$1',
-  }),
-);
 server.use(router);
+
 server.listen(PORT, () => {
   console.log('Server is running');
 });
